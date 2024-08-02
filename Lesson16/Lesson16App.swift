c9919ab9ad42bddf71cec53007d3f7b5a71f5923
Sergey Dyatlov <sergeyd020501@gmail.com>
2024-08-02 19:46:26 +0300

    Initial Commit

---
+++ Lesson16/Lesson16App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson16App.swift
+//  Lesson16
+//
+//  Created by Сергей Дятлов on 02.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Lesson16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
